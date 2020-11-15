74b5a964c92668afb4869a9152b7093cf7be5e88
Palmer Cluff <palmercluff@gmail.com>
2020-11-14 22:48:05 -0700

    Correcting missplaced font-awesome footer icons and text

--- environment-setup.sh
+++ environment-setup.sh
@@ -3,3 +3,6 @@ sudo apt-get install jekyll
 # jekyll new ./japaniku-jekyll
 # cd japaniku-jekyll
 # jekyll serve
+
+# Using the font-awesome package from official repo
+# sudo apt-get install fonts-font-awesome
